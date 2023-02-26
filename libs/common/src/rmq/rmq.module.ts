b90af59a35a6ea1274fadfcdfbd1c28132c1ef55
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-02-26 20:12:05 +0900

    no message

--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -18,12 +18,16 @@ export class RmqModule {
       imports: [
         ClientsModule.registerAsync([
           {
+            //동적모듈로 OdersModule에 들어갈 때 이름을 배당받음
             name,
             useFactory: (configService: ConfigService) => ({
               transport: Transport.RMQ,
               options: {
                 urls: [configService.get<string>('RABBIT_MQ_URI')],
                 queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+                queueOptions: {
+                  durable: true
+                },
               },
             }),
             inject: [ConfigService],
