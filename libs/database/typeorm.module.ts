e5a87984eb6fdec8f1f4536e87bef9fc34bd1167
Lsp <spyro9282@gmail.com>
2023-02-27 17:31:13 +0900

    add user table rleation set

--- libs/database/typeorm.module.ts
+++ libs/database/typeorm.module.ts
@@ -5,6 +5,8 @@ import * as path from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from 'libs/entity/user.entity';
 import { join } from 'path';
+import { Product } from 'libs/entity/product.entity';
+import { Order } from 'libs/entity/order.entity';
 
 const entityPath = path.join(__dirname, 'libs/entity/**/*.entity.ts');
 @Module({
@@ -23,7 +25,7 @@ const entityPath = path.join(__dirname, 'libs/entity/**/*.entity.ts');
         username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: ['libs/entity/**/*.entity.ts', User],
+        entities: [entityPath, User, Product, Order],
         synchronize: false,
         logging: true,
       }),
