1950ad355f5137fc3df6bd15ce51bcf1568cd2b2
Lsp <spyro9282@gmail.com>
2023-03-09 20:13:32 +0900

    RabbitMQ URI Balancing

--- libs/common/src/rmq/rmq.connection.ts
+++ libs/common/src/rmq/rmq.connection.ts
@@ -6,27 +6,53 @@ import rabbitmqConfig from './rmq.conf';
 @Injectable()
 export class RabbitmqChannelProvider {
   constructor(private readonly configService: ConfigService) {}
-
+  private x = 'A';
   async createChannel(): Promise<Channel> {
     try {
-      const rabbitmqUrl = this.configService.get<string>('RABBIT_MQ_URI');
+      const balancing = () => {
+        switch (this.x) {
+          case 'A':
+            this.x = 'B';
+            return 'A';
+          case 'B':
+            this.x = 'C';
+            return 'B';
+          case 'C':
+            this.x = 'A';
+            return 'C';
+        }
+      };
+
+      const URI = balancing();
+
+      const rabbitmqUrl = this.configService.get<string>(
+        `RABBIT_MQ_URI_${URI}`,
+      );
       const connection = await connect(rabbitmqUrl);
       const channel = await connection.createChannel();
-      
+
       // Declare exchanges
       for (const exchange of rabbitmqConfig.exchanges) {
-        await channel.assertExchange(exchange.name, exchange.type, exchange.options);
-      }
-      
-      // Declare queues and bind them to exchanges
-      for (const queue of rabbitmqConfig.queues) {
-        await channel.assertQueue(queue.name, queue.options);
-        for (const binding of queue.bindings) {
-          await channel.bindQueue(queue.name, binding.exchange, binding.routingKey);
-        }
-      }
+        await channel.assertExchange(
+          exchange.name,
+          exchange.type,
+          exchange.options,
+        );
 
-      return channel;
+        // Declare queues and bind them to exchanges
+        for (const queue of rabbitmqConfig.queues) {
+          await channel.assertQueue(queue.name, queue.options);
+          for (const binding of queue.bindings) {
+            await channel.bindQueue(
+              queue.name,
+              binding.exchange,
+              binding.routingKey,
+            );
+          }
+        }
+
+        return channel;
+      }
     } catch (e) {
       console.log(e);
       throw new Error(e.response);
